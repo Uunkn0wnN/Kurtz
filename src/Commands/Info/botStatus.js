2bd7cdeaf0ba80ff11f7d53c38a1d271bdb4d064
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-07-27 19:30:30 +0700

    Update botStatus.js

--- src/Commands/Info/botStatus.js
+++ src/Commands/Info/botStatus.js
@@ -29,7 +29,7 @@ module.exports = {
             .setThumbnail(client.user.displayAvatarURL({ size: 512, format: 'png' }))
             .setColor(message.guild.me.displayHexColor || client.color)
             .addField(
-                '<a:bot:863216970554933269> General',
+                'General',
                 `**❯ Client :** ${client.user.tag} (${client.user.id})\n**❯ Commands Total :** ${
                     client.commands.size
                 }\n**❯ Server :** ${client.guilds.cache.size.toLocaleString()} Servers\n**❯ Users :** ${client.guilds.cache
@@ -43,14 +43,14 @@ module.exports = {
                 )}`
             )
             .addField(
-                '<:ubuntu:853158810654343208> System',
+                'System',
                 `**❯ OS Platform :** ${capitalizeFirst(process.platform)}\n**❯ OS Uptime :** ${ms(os.uptime() * 1000, {
                     long: true,
                 })}\n**❯ CPU :**\n\u3000 Cores : ${os.cpus().length}\n\u3000 Model : ${core.model}\n\u3000 Speed : ${
                     core.speed
                 } MHz`
             )
-            .addField('<:stagechannel:863214920548089866> Network', `**❯ Latency :** ${client.ws.ping} ms`)
+            .addField('Network', `**❯ Latency :** ${client.ws.ping} ms`)
             .setTimestamp();
 
         message.channel.send({ embeds: [embed] });

1b98f1f18b82bbeafce95ccb160fb74a29ae900f
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-08-01 10:00:37 +0700

    Update snake.js

--- src/Commands/Games/snake.js
+++ src/Commands/Games/snake.js
@@ -5,7 +5,7 @@ module.exports = {
     name: 'snake',
     description: 'Play snake game',
     aliases: ['snakecord'],
-    emoji: '🐍',
+    emoji: '**>** ',
     userperm: ['SEND_MESSAGES'],
     botperm: ['SEND_MESSAGES'],
     /**

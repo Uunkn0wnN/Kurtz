7445758e75d6ecab4a589ec03bafa3ed4877269e
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-08-03 12:01:17 +0700

    Update youtube-together.js

--- src/Commands/Utility/youtube-together.js
+++ src/Commands/Utility/youtube-together.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'youtube-together',
     description: 'Start YouTube Together Activity in a Voice Channel',
     aliases: ['ytt'],
-    emoji: '👥',
+    emoji: '**>** ',
     userperm: ['SEND_MESSAGES', 'CONNECT'],
     botperm: ['SEND_MESSAGES', 'CONNECT'],
     /**

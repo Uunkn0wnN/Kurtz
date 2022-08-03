364989d8a840c5aa53d502fdfcb27e9c8ef1b263
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-08-03 12:09:11 +0700

    Update ReactOnSuggest.js

--- src/Events/ReactOnSuggest.js
+++ src/Events/ReactOnSuggest.js
@@ -1,11 +1,11 @@
 const client = require('../../index');
 
-const channel = '849139267471933502';
+const channel = '882810870508433411';
 client.on('messageCreate', message => {
     if (channel.includes(message.channel.id)) {
         if (message.author.bot) {
-            message.react('👍');
-            message.react('👎');
+            message.react('🔼');
+            message.react('🔽');
             message.react('🤷');
         }
         if (!message.author.bot) return;

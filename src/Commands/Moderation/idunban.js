774d31e952c007ff1a8ce46a684393d57dd37843
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-08-06 09:23:54 +0700

    Create idunban.js

---
+++ src/Commands/Moderation/idunban.js
@@ -0,0 +1,27 @@
+const { Client, Message, MessageEmbed } = require('discord.js')
+
+module.exports = {
+    name: 'idunban',
+    description: 'UnBan a user with their ID',
+    emoji: '**>** ',
+    userperm: ['BAN_MEMBERS', 'SEND_MESSAGES'],
+    botperm: ['BAN_MEMBERS', 'SEND_MESSAGES'],
+    run: async (client, message, args) => {
+        
+        if (!message.member.permissions.has("BAN_MEMBERS")) return message.reply("**You Don't Have The Permission To ban A User!**");
+
+        if (!args[0]) return message.reply("You need to specify a user ID");
+
+      let user = args[0];
+      let reden = args[1] || 'No reason given.'
+        
+      let ban = await message.guild.members.unban(user, {
+        reden
+      })
+          const embed1 = new MessageEmbed()
+          .setColor('#8BED7F')
+          .setTitle(`:white_check_mark: Banned ${ban.tag || "Unknown User"}`)
+          .setDescription(`Reason: ${reden}`)
+          return message.reply({ embeds: [embed1]});
+        },
+      };

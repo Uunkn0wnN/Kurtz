b7e58d4f4ff3d51c9628c231a13f14ce37312b69
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-08-15 13:18:14 +0700

    Update invite.js

--- src/Commands/Info/invite.js
+++ src/Commands/Info/invite.js
@@ -2,7 +2,7 @@ const { Client, Message, MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: 'invite',
-    description: 'Invite Exxyll Today!',
+    description: 'Invite Kurtz Today!',
     emoji: '**>** ',
     userperm: ['SEND_MESSAGES'],
     botperm: ['SEND_MESSAGES'],
@@ -17,7 +17,7 @@ module.exports = {
             .setThumbnail(client.user.displayAvatarURL({ dynamic: true, size: 256 }))
             .setTitle('Invite me to your server!')
             .setDescription(
-                `[Invite and Authorize](https://discord.com/oauth2/authorize?client_id=900035170659360861&permissions=1644971949559&scope=applications.commands%20bot) | [Visit Website](https://shiroweb-dashboard.221447.repl.co) | [Join Support Server](https://discord.gg/UFTXkxRrWT)\n\n[Github](https://github.com/Uunkn0wnN)`
+                `[Invite and Authorize](https://discord.com/api/oauth2/authorize?client_id=897359232117133324&permissions=1644971949559&scope=bot%20applications.commands) | [Visit Website](https://shiroweb-dashboard.221447.repl.co) | [Join Support Server](https://discord.gg/UFTXkxRrWT)\n\n[Github](https://github.com/Uunkn0wnN)`
             )
             .setFooter(`${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }));
 

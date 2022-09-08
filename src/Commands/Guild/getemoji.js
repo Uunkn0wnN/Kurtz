5d60d02b079b53ffae569e371b867f1336fb9a25
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-09-08 08:48:55 +0700

    Update getemoji.js

--- src/Commands/Guild/getemoji.js
+++ src/Commands/Guild/getemoji.js
@@ -7,7 +7,7 @@ module.exports = {
     const discord = require("discord.js");
     if (!args[0]) return message.reply({content: 'type the emoji', allowedMentions: { repliedUser: false } });
     const lro = discord.Util.parseEmoji(args[0]);
-    if ('!lro.name || !lro.id') return message.reply({content: 'can't find this emoji'});
+    if ("!lro.name || !lro.id") return message.reply({content: 'can't find this emoji'});
     const image = `${lro.name}: https://cdn.discordapp.com/emojis/${lro.id}.${lro.animated ? "gif" : "png"}`;
     message.reply(image);
   }

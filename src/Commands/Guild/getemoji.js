c7562041daf4cc2e1ae896f0db0de244b9094657
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-09-08 08:43:42 +0700

    Create getemoji.js

---
+++ src/Commands/Guild/getemoji.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: 'getemoji',
+  description: 'Get Emoji Url link',  
+  aliases: ['getemoji'],
+  emoji: '**>** ',  
+  run: async(client, message, args) => {
+    const discord = require("discord.js");
+    if (!args[0]) return message.reply({content: 'type the emoji', allowedMentions: { repliedUser: false } });
+    const lro = discord.Util.parseEmoji(args[0]);
+    if (!lro.name || !lro.id) return message.reply({content: 'can't find this emoji'});
+    const image = `${lro.name}: https://cdn.discordapp.com/emojis/${lro.id}.${lro.animated ? "gif" : "png"}`;
+    message.reply(image);
+  }
+     }   

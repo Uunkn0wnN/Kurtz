c856dd88ab1672b8b14084b4aaaa1c0634150026
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-09-10 10:42:58 +0700

    Update Ready.js

--- src/Events/Ready.js
+++ src/Events/Ready.js
@@ -1,48 +1,17 @@
-const figlet = require('figlet');
+require('dotenv').config;
+const client = require('../../index');
 const chalk = require('chalk');
+const prefix = process.env.PREFIX;
 
-module.exports = async (client) => {
-    client.manager.init(client.user.id);
-    figlet(client.user.tag, function(err, data) {
-        if (err) {
-            console.log('Something went wrong...');
-            console.dir(err);
-            return;
-        }
-        console.log(chalk.red.bold(data));
+client.on('ready', async () => {
+    client.user.setActivity(`PEKOB.NET | Live at ${client.guilds.cache.size} Servers`, {
+        type: 'COMPETING',
+        url: 'https://www.twitch.tv/discord.gg/UFTXkxRrWT',
     });
 
-    let guilds = client.guilds.cache.size;
-    let users = client.users.cache.size;
-    let channels = client.channels.cache.size;
-
-    const activities = [
-        `k!help | ${guilds} servers | ${channels} channels | ${client.guilds.cache.reduce((users , value) => users + value.memberCount, 0)} user`,
-        `Shiro.help`,
-        `k!help | ${guilds} servers | ${channels} channels | ${client.guilds.cache.reduce((users , value) => users + value.memberCount, 0)} user`,
-        `Shiro music`,
-        `k!help | ${guilds} servers | ${channels} channels | ${client.guilds.cache.reduce( ( users , value ) => users + value.memberCount, 0)} user`,
-        `https://discord.gg/UFTXkxRrWT`,
-        `k!help | ${guilds} servers | ${channels} channels | ${client.guilds.cache.reduce((users , value) => users + value.memberCount, 0)} user`,
-        `https://github.com/Uunkn0wnN `,
-        `k!help | ${guilds} servers | ${channels} channels | ${client.guilds.cache.reduce((users , value) => users + value.memberCount, 0)} user`,
-        `https://shiroweb-dashboard.221447.repl.co`,
-        `k!help | ${guilds} servers | ${channels} channels | ${client.guilds.cache.reduce((users , value) => users + value.memberCount, 0)} user`,
-        `YOU`,
-        `k!help | ${guilds} servers | ${channels} channels | ${client.guilds.cache.reduce((users , value) => users + value.memberCount, 0)} user`,
-        `MY SELF`,
-        `k!help | ${guilds} servers | ${channels} channels | ${client.guilds.cache.reduce((users , value) => users + value.memberCount, 0)} user`,
-        `スマイル`,
-        `k!help | ${guilds} servers | ${channels} channels | ${client.guilds.cache.reduce((users , value) => users + value.memberCount, 0)} user`,
-        `kontol`,
-        `PEKOB.NET`,
-        `anjing lu`,
-        `Nazarick Clan`,
-       `k!help | ${guilds} servers | ${channels} channels | ${users} owner`,
-    ]
-
-    setInterval(() => {
-        client.user.setActivity(`${activities[Math.floor(Math.random() * activities.length)]}`, { type: "STREAMING", url: "https://www.twitch.tv/RelaxBeats" });
-    }, 13000)
-
-};
+    console.log(
+        `${chalk.grey.bold('[INFO]  ')}${chalk.blueBright.bold(client.user.tag)} ${chalk.white(
+            'is'
+        )} ${chalk.green.bold('Online')}`
+    );
+});

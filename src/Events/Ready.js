9c99af0d5bf7b13f1f575c10c8928ada8695ce73
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-09-10 10:35:35 +0700

    Create Ready.js

--- src/Events/Ready.js
+++ src/Events/Ready.js
@@ -1,17 +1,48 @@
-require('dotenv').config;
-const client = require('../../index');
+const figlet = require('figlet');
 const chalk = require('chalk');
-const prefix = process.env.PREFIX;
 
-client.on('ready', async () => {
-    client.user.setActivity(`PEKOB.NET | Live at ${client.guilds.cache.size} Servers`, {
-        type: 'COMPETING',
-        url: 'https://www.twitch.tv/discord.gg/UFTXkxRrWT',
+module.exports = async (client) => {
+    client.manager.init(client.user.id);
+    figlet(client.user.tag, function(err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(chalk.red.bold(data));
     });
 
-    console.log(
-        `${chalk.grey.bold('[INFO]  ')}${chalk.blueBright.bold(client.user.tag)} ${chalk.white(
-            'is'
-        )} ${chalk.green.bold('Online')}`
-    );
-});
+    let guilds = client.guilds.cache.size;
+    let users = client.users.cache.size;
+    let channels = client.channels.cache.size;
+
+    const activities = [
+        `k!help | ${guilds} servers | ${channels} channels | ${client.guilds.cache.reduce((users , value) => users + value.memberCount, 0)} user`,
+        `Shiro.help`,
+        `k!help | ${guilds} servers | ${channels} channels | ${client.guilds.cache.reduce((users , value) => users + value.memberCount, 0)} user`,
+        `Shiro music`,
+        `k!help | ${guilds} servers | ${channels} channels | ${client.guilds.cache.reduce( ( users , value ) => users + value.memberCount, 0)} user`,
+        `https://discord.gg/UFTXkxRrWT`,
+        `k!help | ${guilds} servers | ${channels} channels | ${client.guilds.cache.reduce((users , value) => users + value.memberCount, 0)} user`,
+        `https://github.com/Uunkn0wnN `,
+        `k!help | ${guilds} servers | ${channels} channels | ${client.guilds.cache.reduce((users , value) => users + value.memberCount, 0)} user`,
+        `https://shiroweb-dashboard.221447.repl.co`,
+        `k!help | ${guilds} servers | ${channels} channels | ${client.guilds.cache.reduce((users , value) => users + value.memberCount, 0)} user`,
+        `YOU`,
+        `k!help | ${guilds} servers | ${channels} channels | ${client.guilds.cache.reduce((users , value) => users + value.memberCount, 0)} user`,
+        `MY SELF`,
+        `k!help | ${guilds} servers | ${channels} channels | ${client.guilds.cache.reduce((users , value) => users + value.memberCount, 0)} user`,
+        `スマイル`,
+        `k!help | ${guilds} servers | ${channels} channels | ${client.guilds.cache.reduce((users , value) => users + value.memberCount, 0)} user`,
+        `kontol`,
+        `PEKOB.NET`,
+        `anjing lu`,
+        `Nazarick Clan`,
+       `k!help | ${guilds} servers | ${channels} channels | ${users} owner`,
+    ]
+
+    setInterval(() => {
+        client.user.setActivity(`${activities[Math.floor(Math.random() * activities.length)]}`, { type: "STREAMING", url: "https://www.twitch.tv/RelaxBeats" });
+    }, 13000)
+
+};

c428bd3db3e48172465460abba1c18efcfe95629
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-09-13 08:41:03 +0700

    Update Ready.js

--- src/Events/Ready.js
+++ src/Events/Ready.js
@@ -1,1 +1,17 @@
-const activities = [`k!help`, `test`];   setInterval(() => {     let activity = activities[Math.floor(Math.random() * activities.length)];     client.user.setActivity(activity, { type: "PLAYING" })   }, 7000); })
+require('dotenv').config;
+const client = require('../../index');
+const chalk = require('chalk');
+const prefix = process.env.PREFIX;
+
+client.on('ready', async () => {
+    client.user.setActivity(`PEKOB.NET | Live at ${client.guilds.cache.size} Servers`, {
+        type: 'STREAMING',
+        url: 'https://instagram.com/22.1447?igshid=YmMyMTA2M2Y=',
+    });
+
+    console.log(
+        `${chalk.grey.bold('[INFO]  ')}${chalk.blueBright.bold(client.user.tag)} ${chalk.white(
+            'is'
+        )} ${chalk.green.bold('Online')}`
+    );
+});

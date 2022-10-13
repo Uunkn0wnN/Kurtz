96e731592ba90b652fe0b0f48aaaf420e3a684f1
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-10-13 20:01:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -46,6 +46,30 @@ client.on("ready", async c => {
 }, 1000 * 60);
 });
 
+client.on("messageCreate", message =>{
+if(message.author.bot) return
+if(message.content.startsWith(prefix + "Hook")){
+let args = message.content.split(" ").slice(2).join(" ")
+let user = message.mentions.members.first()
+  if(user){
+if(args){
+message.channel.createWebhook(user.user.username, {
+  avatar: user.user.avatarURL(),
+  reason: 'code by スマイル'
+})
+  .then(w=>{w.send(args)
+setTimeout (function aa(){w.delete()},1000)
+})}
+else{
+  message.reply("Provide a message")
+}}
+else{
+  message.reply("mention a user")
+}
+}
+  
+})
+
 // Initialize project
 require('./src/Handler')(client);
 

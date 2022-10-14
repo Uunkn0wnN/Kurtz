0822103254149368fd4c51ca9fa4898a345366c6
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-10-14 07:00:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -46,17 +46,6 @@ client.on("ready", async c => {
 }, 1000 * 60);
 });
 
-client.on("messageCreate", message =>{
-if(message.author.bot) return
-if(message.content.startsWith(prefix + "Hook")){
-let args = message.content.split(" ").slice(2).join(" ")
-let user = message.mentions.members.first()
-  if(user){
-if(args){
-message.channel.createWebhook(user.user.username, {
-  avatar: user.user.avatarURL(),
-  reason: 'code by スマイル'
-})
   .then(w=>{w.send(args)
 setTimeout (function aa(){w.delete()},1000)
 })}
